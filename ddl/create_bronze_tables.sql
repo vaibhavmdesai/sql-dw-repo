bd72b2538983ba25afee926b63caf8a8719721a9
Vaibhav Desai <vaibhavdesai90@gmail.com>
2025-06-30 15:46:47 +0530

    Added bronze tables

--- ddl/create_bronze_tables.sql
+++ ddl/create_bronze_tables.sql
@@ -22,7 +22,7 @@ prd_start_dt date,
 prd_end_dt date
 );
 
-create table belong_iceberg_catalog.bronze.sales_details
+create table belong_iceberg_catalog.bronze.crm_sales_details
 (
 sls_ord_num varchar,
 sls_prd_key varchar,
