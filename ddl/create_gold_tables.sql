3383bef043b7a8d5711c45ac6a6407cc782456c7
Vaibhav Desai <vaibhavdesai90@gmail.com>
2025-07-07 09:36:43 +0530

    added gold products

--- ddl/create_gold_tables.sql
+++ ddl/create_gold_tables.sql
@@ -12,3 +12,20 @@ birth_date date,
 create_date date,
 dwh_load_date timestamp(6)
 )
+
+
+CREATE TABLE belong_iceberg_catalog.gold.dim_products
+(
+product_key integer,
+product_id integer,
+product_number varchar,
+product_name varchar,
+category_id varchar,
+category varchar,
+subcategory varchar,
+maintenance varchar,
+cost double,
+product_line varchar,
+start_date varchar,
+dwh_load_date timestamp(6)
+);
